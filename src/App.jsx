9e7957d737e4a2e0791a5af35c43f576b186d848
Sarahberkersuzan <sarah.berkersuzan@etu.unilim.fr>
2026-09-04 11:23:46 +0200

    install

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const name = "Hedy Lamarr";
+function App() {
+  return (
+  <div >
+    <h1>Hedy Lamarr's Todos</h1>
+    <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Hedy Lamarr" />
+    <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehearse a movie scene</li>
+      <li>Improve the spectrum technology</li>
+    </ul>
+  </div>
+  );
+}
+
+export default App;
